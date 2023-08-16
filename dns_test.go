50df2480d514597539972c69ed868a240795f49c
paul-kang-1 <loiter97@gmail.com>
2023-08-16 14:19:33 -0400

    improved tests

--- dns_test.go
+++ dns_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,4 +18,27 @@ func TestEncodeDomainName(t *testing.T) {
 	assert.EqualValues(t, res[4:7], "com")
 	// Check null termination
 	assert.EqualValues(t, res[7], 0)
+}
+
+func TestPacketFromBytes(t *testing.T) {
+	// Raw packet file retrieved via:
+	// dig @a.root-servers.net google.com +norecurse
+	file, err := os.Open("testfiles/packet.bin")
+	assert.NoError(t, err)
+	b := make([]byte, 1024)
+	_, err = file.Read(b)
+	assert.NoError(t, err)
+	reader := bytes.NewReader(b)
+	var packet DNSPacket
+	assert.NoError(t, packet.FromBytes(reader))
+	// Check questions
+	qs := *packet.Questions
+	assert.Equal(t, string(qs[0].Name), "www.google.com")
+}
+
+func TestSendQuery(t *testing.T) {
+	packet, err := SendQuery("198.41.0.4", "localhost", TypeA)
+	assert.NoError(t, err)
+	answers := *packet.Answers
+	assert.EqualValues(t, answers[0].Data, []byte{127, 0, 0, 1})
 }
